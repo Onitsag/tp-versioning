38438baea6529535048a3ca497ff29f51dda6abd
Timéo Gastineau <tgastineau@dombtsig.local>
2022-09-14 14:55:54 +0200

    Setup MVC 2

--- views/global/home.php
+++ views/global/home.php
@@ -1,13 +1,28 @@
-<div class="container mt-5">
-    <div class="row">
-        <div class="card">
-            <div class="card-body">
-                <h3>Site de démonstration</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, commodi delectus eaque eos esse ex illum laboriosam laborum modi, molestias necessitatibus non provident quidem quis repellat soluta tempore, veritatis vitae.
-                </p>
-                <div class="text-center">Page générée le <?= $date ?></div>
-            </div>
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <div id="container">
+            <!-- zone de connexion -->
+            
+            <form action="verification.php" method="POST">
+                <h1>Connexion</h1>
+                
+                <label><b>Nom d'utilisateur</b></label>
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+                <input type="submit" id='submit' value='LOGIN' >
+                <?php
+                // Code de vérification 
+                ?>
+            </form>
+            <p><a href="inscription.php">S'inscrire</p>
         </div>
-    </div>
-</div>
+    </body>
+</html>
